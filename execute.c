f8c4ef3ccbc8eb586e9344fbdfb373e22edd3b49
Chiefbuma <122814097+Chiefbuma@users.noreply.github.com>
2024-01-21 19:08:03 +0300

    Update execute.c

--- execute.c
+++ execute.c
@@ -1,9 +1,9 @@
 #include "monty.h"
 /**
-* execute - executes
+* execute - executes the opcode
 * @stack: head linked list - stack
 * @counter: line_counter
-* @file: poiner
+* @file: poiner to monty file
 * @content: line content
 * Return: no return
 */
@@ -27,22 +27,22 @@ int execute(char *content, stack_t **stack, unsigned int counter, FILE *file)
 				{"stack", f_stack},
 				{NULL, NULL}
 				};
-	unsigned int k = 0;
+	unsigned int i = 0;
 	char *op;
 
 	op = strtok(content, " \n\t");
 	if (op && op[0] == '#')
 		return (0);
 	bus.arg = strtok(NULL, " \n\t");
-	while (opst[k].opcode && op)
+	while (opst[i].opcode && op)
 	{
-		if (strcmp(op, opst[k].opcode) == 0)
-		{	opst[k].f(stack, counter);
+		if (strcmp(op, opst[i].opcode) == 0)
+		{	opst[i].f(stack, counter);
 			return (0);
 		}
-		k++;
+		i++;
 	}
-	if (op && opst[k].opcode == NULL)
+	if (op && opst[i].opcode == NULL)
 	{ fprintf(stderr, "L%d: unknown instruction %s\n", counter, op);
 		fclose(file);
 		free(content);
